721b94925e929023f6e27227fa78e7e4b71393ad
Baptiste Chaudron <chaudron964@gmail.com>
2026-01-09 16:43:15 +0100

    feat: bridge between route and template

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 admin_bp = Blueprint("admin", __name__)
 
 @admin_bp.route("/")
 def home():
-    return "Admin Page"
+    return render_template('admin/dashboard')
 
-@admin_bp.route("/articles/add")
+@admin_bp.route("/article/add")
 def add_article():
-    return "Add article"
+    return render_template('admin/add_article.html')
 
-@admin_bp.route("/articles/<article_id>/edit")
+@admin_bp.route("/article/<article_id>/edit")
 def edit_article(article_id: int):
-    return f"Edit article{article_id}"
+    return render_template('admin/edit_article.html', article_id=article_id)
--- app/public_pages/routes.py
+++ app/public_pages/routes.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 public_bp = Blueprint("public", __name__)
 
 @public_bp.route("/")
 def home():
-    return "Hello World"
+    return render_template("home.html")
 
 @public_bp.route("/article/<article_id>")
 def article(article_id: int):
-    return f"Article {article_id}"
+    return render_template("article.html", article_id=article_id)
