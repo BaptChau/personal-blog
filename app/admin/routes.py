35e1cb5f4701efe325b5d36b982001f58b91d018
Baptiste Chaudron <chaudron964@gmail.com>
2026-01-09 16:29:35 +0100

    feat: all routes for the base project

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -5,3 +5,11 @@ admin_bp = Blueprint("admin", __name__)
 @admin_bp.route("/")
 def home():
     return "Admin Page"
+
+@admin_bp.route("/articles/add")
+def add_article():
+    return "Add article"
+
+@admin_bp.route("/articles/<article_id>/edit")
+def edit_article(article_id: int):
+    return f"Edit article{article_id}"
--- app/public_pages/routes.py
+++ app/public_pages/routes.py
@@ -5,3 +5,7 @@ public_bp = Blueprint("public", __name__)
 @public_bp.route("/")
 def home():
     return "Hello World"
+
+@public_bp.route("/article/<article_id>")
+def article(article_id: int):
+    return f"Article {article_id}"
