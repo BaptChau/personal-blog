c3467d3df0be41f94ae61bd56831e3dc699b15b1
Baptiste Chaudron <chaudron964@gmail.com>
2026-01-09 16:24:59 +0100

    feat: add basic route and modules structures

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "ma-cle-secrete"
+    from app.public_pages.routes import public_bp
+    from app.admin.routes import admin_bp
+
+    app.register_blueprint(public_bp)
+    app.register_blueprint(admin_bp, url_prefix="/admin")
+    return app
