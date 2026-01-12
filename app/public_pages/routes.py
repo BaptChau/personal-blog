c65d613befbacaedba23859aecbc1b65fc54b43d
Baptiste CHAUDRON <chaudron964@gmail.com>
2026-01-12 11:01:34 +0100

    feat: public pages done

--- app/public_pages/routes.py
+++ app/public_pages/routes.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, render_template
-
+from app.admin import helper
 public_bp = Blueprint("public", __name__)
 
 @public_bp.route("/")
 def home():
-    return render_template("home.html")
+    articles = helper.get_all_articles()
+    return render_template("home.html", articles=articles)
 
 @public_bp.route("/article/<article_id>")
 def article(article_id: int):
-    return render_template("article.html", article_id=article_id)
+    article = helper.get_article_by_id(article_id)
+    return render_template("article.html", article=article)
